9a4d40471aa304f20e6f551586b5aaca426b60de
dusanerdeljan <dusanerdeljan99@gmail.com>
2020-03-25 11:40:39 +0100

    Added license.

--- src/game/factory/PipeFactory.java
+++ src/game/factory/PipeFactory.java
@@ -1,3 +1,23 @@
+/*
+Using artifical neural network and genetic algorithm to train bot to play Flappy Bird
+Copyright (C) 2020 Dušan Erdeljan
+
+This file is part of neuroevolution-flappy-bird
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
 package game.factory;
 
 import java.util.Random;
